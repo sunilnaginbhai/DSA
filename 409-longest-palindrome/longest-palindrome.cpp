850a5fd54d5c9c9acc2bd6c15a3c931f16362f0a
patel sunil <sunilnaginbhai@gmail.com>
2026-08-30 08:08:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (5.02%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int n=s.length();
+        unordered_set<char> st;
+
+        int result=0;
+
+        for(char &ch:s){
+            if(st.count(ch)){
+                result+=2;
+                st.erase(ch);
+            }else{
+                st.insert(ch);
+            }
+        }
+
+        if(!st.empty()){
+            result++;
+        }
+
+        return result;
+    }
+};
