77b3f16d94c78f09833463f9ecbcda7b0f7f41e0
patel sunil <sunilnaginbhai@gmail.com>
2026-09-08 08:51:40 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (78.06%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -6,41 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-
-        if (!root)
-            return result;
-
+        vector<vector<int>> ans;
         queue<TreeNode*> q;
+        if(root==NULL) return ans;
         q.push(root);
-        bool leftToRight = true;
+        bool isreverse=true;
+        while(!q.empty()){
+            int n=q.size();
+            vector<int> travers;
 
-        while (!q.empty()) {
-            int size = q.size();
-
-            vector<int> level(size);
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-
-                int index=leftToRight? i : (size-1-i);
-                level[index]=node->val;
-                
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+            for(int i=0;i<n;i++){
+               TreeNode* node=q.front();
+               q.pop();
+               if(node->left!=NULL) q.push(node->left);
+               if(node->right!=NULL) q.push(node->right);
+               travers.push_back(node->val);
             }
-            leftToRight=!leftToRight;
-
-             result.push_back(level);
+           if(isreverse){
+           isreverse=!isreverse;
+           }else{
+            reverse(travers.begin(),travers.end());
+            isreverse=!isreverse;
+           }
+            ans.push_back(travers);
         }
-        return result;
+        return ans;
     }
 };
